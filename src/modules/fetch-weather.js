df5856d66b2ddabfbbdccc36b8b13f05cd11c3e8
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-11-26 18:41:44 -0300

    Refactor gettingCity function

--- src/modules/fetch-weather.js
+++ src/modules/fetch-weather.js
@@ -1,6 +1,7 @@
 import cloudy from '../assets/cloudy.jpg';
 import rainyDay from '../assets/rainy-day.jpg';
 import sunny from '../assets/sunny.jpg';
+import weatherSpec from './weather-specific';
 
 const gettingWeather = (city) => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=bde3f0994e6528c00d440747db847b86`,
@@ -8,7 +9,7 @@ const gettingWeather = (city) => {
     ).then(response => response.json())
     .then( response => {
         console.log(response);
-
+        
         if (response.weather[0].main === 'Clouds') {
             document.body.style.backgroundImage = `url(${cloudy})`;
         } else if (response.weather[0].main === 'Clear'){
@@ -16,6 +17,8 @@ const gettingWeather = (city) => {
         } else if (response.weather[0].main === 'Rain') {
             document.body.style.backgroundImage = `url(${rainyDay})`;
         }
+
+        weatherSpec(response);
     })
     .catch(error => {
         console.log(error);
---
+++ src/modules/weather-specific.js
@@ -0,0 +1,17 @@
+import weatherContainer from "./weather-container";
+
+const weatherSpecifics = (response) => {
+   const mainContainer = document.querySelector('.content-cont');
+
+   const container = document.createElement('div');
+   container.classList.add('weather-specific');
+
+   const heading = document.createElement('h1');
+   heading.textContent += `Weather in: ${response.name}`;
+   heading.classList.add('text-center', 'font-weight-bold');
+
+   container.append(heading);
+   mainContainer.append(container);
+}
+
+export default weatherSpecifics;
