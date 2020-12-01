bd77e11c759bd060a016190418a40b02eb55ab68
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-12-01 08:00:00 -0300

    Update fetch link

--- src/modules/fetch-weather.js
+++ src/modules/fetch-weather.js
@@ -2,7 +2,7 @@ import weatherSpec from './weather-specific';
 import backgroundChange from './backgr-management';
 
 const gettingWeather = (city) => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=bde3f0994e6528c00d440747db847b86`,
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=bde3f0994e6528c00d440747db847b86`,
     { mode: 'cors' }).then(response => response.json())
     .then(response => {
       backgroundChange(response);
